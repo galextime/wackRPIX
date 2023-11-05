a3025c24e2f1a24f2b5cab50d0aa7e86c4a3c1b3
Alex Grushkin <grusha@rpi.edu>
2023-11-05 03:04:26 -0500

    bruh

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,77 +1,8 @@
-/* Lab 5 JavaScript File 
-   Place variables and functions in this file */
-
-   function validate(formObj) {
-    // put your validation code here
-    // it will be a series of if statements
- 
-    var alertText = "";
-    var focusSet = 0;
- 
-    if (formObj.firstName.value == "") {
-       alertText += "You must enter a first name \n";
-       if (focusSet == 0) {
-          formObj.comments.focus();
-          focusSet = 1;
-       }
-    }
- 
-    if (formObj.lastName.value == "") {
-       alertText += "You must enter a last name \n";
-       if (focusSet == 0) {
-          formObj.comments.focus();
-          focusSet = 1;
-       }
-    }
- 
-    if (formObj.title.value == "") {
-       alertText += "You must enter a title \n";
-       if (focusSet == 0) {
-          formObj.comments.focus();
-          focusSet = 1;
-       }
-    }
- 
-    if (formObj.org.value == "") {
-       alertText += "You must enter an organization \n";
-       if (focusSet == 0) {
-          formObj.comments.focus();
-          focusSet = 1;
-       }
-    }
- 
-    if (formObj.pseudonym.value == "") {
-       alertText += "You must enter a nickname \n";
-       if (focusSet == 0) {
-          formObj.comments.focus();
-          focusSet = 1;
-       }
-    }
- 
-    if (formObj.comments.value == "Please enter your comments") {
-       alertText += "You must enter a comment \n";
-       if (focusSet == 0) {
-          formObj.comments.focus();
-          focusSet = 1;
-       }
-    }
-    
-    if (alertText == "") {
-       alert("Form successfully submitted!");
-       return true;
-    }
- 
-    if (alertText != "") {
-       alert(alertText);
-       return true;
-    }
- }
- 
- function clearText() {
+function clearText() {
  
     var currentText = document.getElementById("comments");
     
-    if (currentText.value == "Please enter a descrption of your conditions") {
+    if (currentText.value == "Please type a description of your condition here") {
        currentText.value = "";
     }
  }
@@ -81,16 +12,13 @@
     var currentText = document.getElementById("comments");
     
     if (currentText.value == "") {
-       currentText.value = "Please enter a descrption of your conditions";
+       currentText.value = "Please type a description of your condition here";
     }
  }
- 
- function nickname() {
-    formObj = document.getElementById("addForm");
-    if (formObj.firstName.value == "" || formObj.lastName.value == "" || form.obj.pseudonym.value == "") {
-       alert("Missing either first name, last name or nickname.")
-    }
-    else {
-       alert(formObj.firstName.value + " " + formObj.lastName.value + " is " + formObj.pseudonym.value);
-    }
+
+ function validate(formObj) {
+    if (confirm("Are you ready to submit your APPLICATION NAME form?") == true) {
+          location.href = '/selectdate.php';
+       } else {
+      }
  }
